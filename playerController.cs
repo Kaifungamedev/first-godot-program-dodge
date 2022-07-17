295a50fb8c0771b9a6ca1bb8d519d6edecca89ba
malakai <kaifungaming@gmail.com>
2022-07-17 15:36:19 -0500

    setup

--- playerController.cs
+++ playerController.cs
@@ -10,7 +10,7 @@ public class playerController : Node2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        GD.Print("Hello World");
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
